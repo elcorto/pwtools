b2d185eb03d8a4eec04fa71dd873e42ce736fc24
Steve Schmerler <mefx@gmx.net>
2009-12-09 18:03:53 +0100

    * Rewrote cmd line test script.

--- test/test_cmdline.py
+++ test/test_cmdline.py
@@ -14,6 +14,26 @@ def system(call):
     proc = sp.Popen(call, shell=True)    
     os.waitpid(proc.pid, 0)
 
+class Test(object):
+    def __init__(self, call, idx, copy_from=None):
+        self.call = call
+        self.idx = idx
+        self.copy_from = copy_from
+    
+    def setup(self, basedir, exe, infile, outfile):
+        self.basedir = basedir 
+        self.dst = os.path.join(self.basedir, str(self.idx))
+        # make cmd line complete
+        self.call = '%s -i %s -o %s ' %(exe, infile, outfile) + self.call
+        self.call += ' -x %s' %self.dst
+        
+    def execute(self):                    
+        if self.copy_from is not None:
+            src = os.path.join(self.basedir, str(self.copy_from))
+            print "[Test]: copy: %s -> %s" %(src, self.dst)
+            shutil.copytree(src, self.dst)
+        system(self.call)
+
 if __name__ == '__main__':
     
     print('\n' + "*"*78)
@@ -24,56 +44,54 @@ if __name__ == '__main__':
         """))
     print("*"*78)
 
+    
+    tests = []
+    #-------------------------------------------------------------------------
+    # define tests here
+    #-------------------------------------------------------------------------
+    # parse, write bin
+    idx = 0
+    tests.append(Test("-p", idx))
+    
+    # read written data, calculate dos
+    idx = 1
+    tests.append(Test("-d -m -M", idx, copy_from=0))
+    
+    # read written data, calculate dos direct, no mirroring
+    idx = 2
+    tests.append(Test("-d -m 0 -M -t 'direct'", idx, copy_from=0))
+                 
+    # new dir, parse, write txt
+    idx = 3
+    tests.append(Test("-p -f txt", idx))
+
+    # read txt, calculate dos
+    idx = 4
+    tests.append(Test("-d -f txt", idx, copy_from=3))
+    
+    # new dir, parse, write bin, dos in one run
+    idx = 5
+    tests.append(Test("-p -d -f txt", idx))
+    #-------------------------------------------------------------------------
+    
+    # exec tests
     infile = "AlN.md.in"
     outfile = "AlN.md.out"
     outdir = "/tmp/test_pdos"
-
-    exe = ' ../lib/pydos.py'
+    exe = '../lib/pydos.py'
     
     # We do no longer support reading .gz files directly.
     system('gunzip ' + outfile + '.gz')
 
-    calls =[]
-    # parse, write bin
-    idx = 0
-    calls.append("%s -i %s -o %s -p -x %s" %(exe, infile, outfile, \
-        os.path.join(outdir, str(idx))))
-    
-    # read written data, calculate dos
-    calls.append("%s -i %s -o %s -d -m -M -x %s" %(exe, infile, outfile,\
-        os.path.join(outdir, str(idx))))
-    
-    # read written data, calculate dos direct, no mirroring
-    calls.append("%s -i %s -o %s -d -m 0 -M -t 'direct' -x %s" %(exe, infile,\
-        outfile, os.path.join(outdir, str(idx))))
-    
-    # new dir, parse, write txt
-    idx += 1
-    calls.append("%s -i %s -o %s -p -f txt -x %s" %(exe, infile, outfile,\
-        os.path.join(outdir, str(idx))))
-    
-    # read txt, calculate dos
-    calls.append("%s -i %s -o %s -d -f txt -x %s" %(exe, infile, outfile,\
-        os.path.join(outdir, str(idx))))
-    
-    # new dir, parse, write bin, dos in one run
-    idx += 1
-    calls.append("%s -i %s -o %s -p -d -f txt -x %s" %(exe, infile, outfile,\
-            os.path.join(outdir, str(idx))))
-    
     if os.path.exists(outdir):
         shutil.rmtree(outdir)
-    
-    for call in calls:
+
+    for test in tests:
         print('\n'+ '+'*78)
-        system(call)
-    
-    if os.path.exists(outdir):
-        shutil.rmtree(outdir)
+        test.setup(outdir, exe, infile, outfile)
+        test.execute()
     
     system('gzip ' + outfile)
-
-    #------------------------------------------------------------------------
 
 ##    print('\n' + "*"*78)
 ##    print("testing import of pwtools package")
