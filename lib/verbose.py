664df1ab209f01ba56e0d6052b77fcb6b9538234
Steve Schmerler <mefx@gmx.net>
2010-03-17 14:08:03 +0100

    * foo

--- lib/verbose.py
+++ lib/verbose.py
@@ -3,7 +3,7 @@
 # Defines message printing stuff. Used in all other modules. Use the global var
 # VERBOSE to turn chatty functions on/off.
 
-VERBOSE=True
+VERBOSE = True
 
 def verbose(msg):
     if VERBOSE:
