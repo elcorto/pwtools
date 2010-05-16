3c4c5e3f2400cb95f399ba35083f07a22cae0635
Steve Schmerler <mefx@gmx.net>
2010-05-16 17:51:07 +0200

    Greatly improved test/test_pdos.py. Now this example should be a complete template for calculating a PDOS.

--- test/test_pdos.py
+++ test/test_pdos.py
@@ -1,27 +1,61 @@
+import sys
+
+import numpy as np
+
 from pwtools.lib.parse import PwOutputFile
 from pwtools import common
 from pwtools import pydos as pd
+from pwtools import constants
 
 filename = 'files/pw.md.out'
 infile = 'files/pw.md.in'
-dumpfile = '/tmp/pw.md.pk'
 
 common.system('gunzip %s.gz' %filename)
 c = PwOutputFile(filename=filename, infile=infile)
 c.parse()
 
-##c.dump(dumpfile)
-##c2 = PwOutputFile()
-##c2.load(dumpfile)
-c2 = c
+# Transform coords if needed. See lib/README.
+ibrav = int(c.infile.namelists['system']['ibrav'])
+c_sys = c.infile.atpos['unit'].lower().strip()
+if c_sys == 'crystal':
+    if ibrav == 0:
+        if c.infile.cell_parameters is None:
+            print "error: no cell parameters in infile, set manually here"
+            sys.exit(1)
+        else:        
+            coords = pd.coord_trans(c.coords, old=c.infile.cell_parameters,
+                                    new=np.identity(3)) 
+    else:
+        print "error: ibrav != 0, cannot get cell parameters from infile \
+              set manually here"
+        sys.exit(1)
+else:
+    coords = c.coords
 
-V = pd.velocity(c2.coords)
-m = c2.infile.massvec
-f, d = pd.direct_pdos(V, m=m)
-##f, d = pd.vacf_pdos(V, m=m, mirr=True)
+# vacf_pdos:
+#   If we compute the *normalized* VCAF, then dt is a factor: 
+#         <v_x(0) v_x(t)> = 1/dt^2 <dx(0) dx(t)> 
+#   which cancels in the normalization. dt is not needed in the velocity
+#   calculation.
+# direct_pdos:
+#   Also here, we do not need V = velocity(coords, dt=dt).
+V = pd.velocity(coords)
+m = c.infile.massvec
+dt = float(c.infile.namelists['control']['dt'])*constants.tryd
+fd, dd = pd.direct_pdos(V, m=m, dt=dt)
+fv, dv = pd.vacf_pdos(V, m=m, dt=dt, mirr=True)
+
+np.testing.assert_array_almost_equal(fd, fv, err_msg="freq not equal")
+np.testing.assert_array_almost_equal(dd, dv, err_msg="dos not equal")
+
+df = fd[1] - fd[0]
+print "Nyquist freq [Hz]: %e" %(0.5/dt)
+print "df [Hz] %e:" %df
 
 from matplotlib import pyplot as plt
-plt.plot(f,d)
+plt.plot(fd, dd, label='direct')
+plt.plot(fv, dv, label='vacf')
+plt.legend()
 plt.show()
 
 common.system('gzip %s' %filename)
