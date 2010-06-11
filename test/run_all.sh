c1f036cee85853d92782bd29a0547d4b66740365
Steve Schmerler <mefx@gmx.net>
2010-06-11 15:55:29 +0200

    Improved run_all.sh to handle OMP_NUM_THREADS.

--- test/run_all.sh
+++ test/run_all.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Simple-minded way to run test suite. Yes, we really should use node.
+# This is for test_f2py_flib_openmp.py
+if env | grep OMP_NUM_THREADS > /dev/null; then
+    old_omp=$OMP_NUM_THREADS
+fi    
+export OMP_NUM_THREADS=3
+
+# Simple-minded way to run test suite. Yes, we really should use nose.
 for f in $(ls -1 *.py); do 
     echo "#################################################################"
     echo "$f"
@@ -12,4 +18,10 @@ if egrep 'error|warning' run_all.log > /dev/null; then
     echo "#################################################################"
     echo "there have been errors/warnings"
     echo "#################################################################"
-fi    
+fi
+
+if [ "$old_omp" != "" ]; then
+    export OMP_NUM_THREADS=$old_omp
+else
+    unset OMP_NUM_THREADS
+fi
