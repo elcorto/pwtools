f3a4aa4f5a50768fe50202019f6d6b56703f4adb
Steve Schmerler <mefx@gmx.net>
2010-06-30 16:19:29 +0200

    Doc update in run_all.sh

--- test/run_all.sh
+++ test/run_all.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Simple script to run all test_*.py . For some tests, some plot windows
+# may pop up. Just close them to continue.
+#
+# For tests calling Fortran extensions: Stdout from Fortran ends up in the
+# wrong order in run_all.log. To see the correct output, run this these tests
+# by hand (python test_foo.py).
+
 # This is for test_f2py_flib_openmp.py
 if env | grep OMP_NUM_THREADS > /dev/null; then
     old_omp=$OMP_NUM_THREADS
@@ -14,13 +21,13 @@ for f in $(ls -1 *.py); do
     python $f
 done 2>&1 | tee run_all.log
 
-if egrep 'error|warning' run_all.log > /dev/null; then
+if egrep -i 'error|warning' run_all.log > /dev/null; then
     echo "#################################################################"
     echo "there have been errors/warnings"
     echo "#################################################################"
 fi
 
-if [ "$old_omp" != "" ]; then
+if [ -n "$old_omp" ]; then
     export OMP_NUM_THREADS=$old_omp
 else
     unset OMP_NUM_THREADS
