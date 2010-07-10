31d2d18c891dd1f7adad9ca3728c54528ffe4cb4
Steve Schmerler <mefx@gmx.net>
2010-07-10 12:03:27 +0200

    Improved version.py : It gets the version string from .../pwtools/.hgtags instead of defining it statically in the file. Pro: Need not define version_str in version.py and commit that. Con: .../pwtools/.hgtags must be present. Added test.

--- lib/version.py
+++ lib/version.py
@@ -13,12 +13,36 @@
 # "use version between 0.5.2 and 0.6.0b1"
 
 from distutils.version import StrictVersion
+##from common import backtick
+import os.path 
 
 def toversion(v1):
     return StrictVersion(v1)
 
+
+def pathback(path, levels=1):
+    p = path
+    for i in range(levels):
+        p = os.path.dirname(p)
+    return p    
+
 # alias 
 tov = toversion
 
-# The highest version number produced by "hg tags".
-version = toversion("0.9.2")
+# version_str : The highest version number, e.g. "0.9.2" produced by "hg tags".
+# It is the last tag before tip. This is the last one listed in .hgtags .
+#     tip                              271:b733e197c96a
+# --> 0.9.2                            268:a4ecac0432a8
+#     0.9.1                            265:82817214abb2
+#     
+##version_str = "0.9.2"
+##version_str = backtick('hg tags | head -n2').split()[2]
+
+hgtags = os.path.join(pathback(__file__, 2), '.hgtags')
+if not os.path.exists(hgtags):
+    print "warning: %s not found" %hgtags
+    version_str = '0.0.0'
+else:
+    version_str = open(hgtags).readlines()[-1].split()[1]
+
+version = toversion(version_str)
---
+++ test/test_version.py
@@ -0,0 +1,5 @@
+from pwtools import version
+from distutils.version import StrictVersion
+
+assert version.version_str != '0.0.0'
+assert version.version != StrictVersion('0.0.0')
