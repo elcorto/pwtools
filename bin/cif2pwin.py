b6b8be65b7ca8da24d39ea791062179076deed65
Steve Schmerler <mefx@gmx.net>
2010-09-01 13:36:02 +0200

    Updated imports in bin/cif2pwin.py .

--- bin/cif2pwin.py
+++ bin/cif2pwin.py
@@ -5,20 +5,19 @@ import numpy as np
 
 from pwtools import parse
 from pwtools import crys
-from pwtools import pydos as pd
+from pwtools.common import str_arr
 
 fn = sys.argv[1]
 pp = parse.CifFile(fn)
 pp.parse()
 
-print "celldm (a[Bohr], b/a, c/a, cos(bc), cos(ac), cos(ab):\n%s\n" %pd.str_arr(pp.celldm)
-print "cell_parameters (div. by a):\n%s\n" %pd.str_arr(pp.cell_parameters / pp.cryst_const[0])
-##print "atpos (crystal):\n%s\n" %pd.atpos_str(pp.symbols, pp.coords)
+print "celldm (a[Bohr], b/a, c/a, cos(bc), cos(ac), cos(ab):\n%s\n" %str_arr(pp.celldm)
+print "cell_parameters (div. by a):\n%s\n" %str_arr(pp.cell_parameters / pp.cryst_const[0])
 print "atpos (crystal):\n%s\n" %pp.atpos_str
 print "natoms:\n%s\n" %pp.natoms
 
 cpr = crys.recip_cp(pp.cell_parameters)
-print "recip. cell_parameters:\n%s\n" %pd.str_arr(cpr)
+print "recip. cell_parameters:\n%s\n" %str_arr(cpr)
 
 norms = np.sqrt((cpr**2.0).sum(axis=1))
 print "relation of recip. vector lengths (a:b:c):\n%s\n" %str(norms/norms.min())
