e78c89598e0c7e0a915e90d611c59687d6bdb717
Steve Schmerler <mefx@gmx.net>
2010-11-12 12:54:40 +0100

    Update test/test_cif.py: Check io.write_cif(), read back in and check consistency of results.

--- test/test_cif.py
+++ test/test_cif.py
@@ -1,9 +1,18 @@
+import os
+import numpy as np
+from pwtools.parse import CifFile
+from pwtools import io
+from testenv import testdir
+
 def test():
-    from pwtools.parse import CifFile
-    from pwtools import common
-
-    c = CifFile('files/cif_struct.cif')
-    c.parse()
-
-    # Note the conversion A -> Bohr in c.celldm[0] == alat.
-    common.print_dct(c.__dict__)
+    p1 = CifFile('files/cif_struct.cif')
+    p1.parse()
+    
+    # test writing
+    filename = os.path.join(testdir, 'test_write_cif.cif')
+    io.write_cif(filename, p1.coords, p1.symbols, p1.cryst_const, conv=False)
+    p2 = CifFile(filename)
+    p2.parse()
+    np.testing.assert_array_almost_equal(p1.coords, p2.coords)
+    np.testing.assert_array_almost_equal(p1.cryst_const, p2.cryst_const)
+    assert p1.symbols == p2.symbols
