1c552a34f6e61829b904973995fcfb0317abdc4a
Steve Schmerler <mefx@gmx.net>
2011-03-23 18:22:01 +0100

    ENH: Add pwscf.ibrav2cell() + test. Finally we got it!

--- pwscf.py
+++ pwscf.py
@@ -6,6 +6,8 @@
 import re
 import numpy as np
 from pwtools.common import fix_eps, str_arr, file_readlines
+from pwtools import crys
+from math import sin, acos, sqrt
 
 def atpos_str(symbols, coords, fmt="%.16e", zero_eps=True):
     """Convenience function to make a string for the ATOMIC_POSITIONS section
@@ -154,3 +156,142 @@ def read_matdyn_freq(filename):
     return kpoints, freqs
 
 
+def ibrav2cell(ibrav, celldm):
+    """Convert PWscf's ibrav + celldm to cell. All formulas are taken straight
+    from the PWscf homepage. Don't blame me for errors. Use after testing. Ask
+    you doctor.
+    
+    `celldm` (a = celldm[0]) is assumed be in the unit that you want for
+    `cell` (Bohr, Angstrom, etc).
+
+    Note: There are some documentation <-> forluma errors / inconsistencies for
+    ibrav=12,13. See test/test_ibrav.py. If you really need that, have a look
+    at the PWscf source for how they do it there.
+
+    args:
+    -----
+    ibrav : int
+        1 ... 14
+    celldm : sequence of length 6
+        This not the isame length 6 array `celldm` in crys.py. Here, the
+        entries which are not needed can be None.
+    
+    returns:
+    --------
+    array (3,3) : cell vectors as rows, unit is that of celldm[0], i.e. a
+
+    notes:
+    ------
+    ibrav = 14 is actually the only case where all 6 entries of `celldm` are
+    needed and therefore the same as crys.cc2cell(crys.celldm2cc(celldm)).
+    The returned `cell` here has the same spatial orientation as the one
+    returned from crys.cc2cell(): a along x, b in xy-plane.
+    """
+    # some of celldm can be None
+    tofloat = lambda x: x if x is None else float(x)
+    aa, bb_aa, cc_aa, cos_alpha, cos_beta, cos_gamma = [tofloat(x) for x in celldm]
+    if bb_aa is not None:
+        bb = bb_aa * aa
+    if cc_aa is not None:
+        cc = cc_aa * aa
+    if cos_gamma is not None:
+        sin_gamma = sin(acos(cos_gamma))
+    if ibrav == 1:
+        # cubic P (sc), sc simple cubic
+        # v1 = a(1,0,0),  v2 = a(0,1,0),  v3 = a(0,0,1)
+        cell = aa * np.identity(3)
+    elif ibrav == 2:
+        # cubic F (fcc), fcc face centered cubic
+        # v1 = a(1,0,0),  v2 = a(0,1,0),  v3 = a(0,0,1)
+        cell = 0.5*aa * np.array([[-1,  0,  1], 
+                                  [ 0,  1,  1], 
+                                  [-1,  1,  0.0]])
+    elif ibrav == 3:
+        # cubic I (bcc), bcc body entered cubic
+        # v1 = (a/2)(1,1,1),  v2 = (a/2)(-1,1,1),  v3 = (a/2)(-1,-1,1)
+        cell = 0.5*aa * np.array([[ 1,  1,  1], 
+                                  [-1,  1,  1], 
+                                  [-1, -1,  1.0]])
+    elif ibrav == 4:
+        # Hexagonal and Trigonal P, simple hexagonal and trigonal(p)
+        # v1 = a(1,0,0),  v2 = a(-1/2,sqrt(3)/2,0),  v3 = a(0,0,c/a)
+        cell = aa * np.array([[ 1,    0,            0], 
+                              [-0.5,  sqrt(3)/2.0,  0], 
+                              [ 0,    0,            cc/aa]])
+    elif ibrav == 5:
+        # Trigonal R, trigonal(r)
+        # v1 = a(tx,-ty,tz),   v2 = a(0,2ty,tz),   v3 = a(-tx,-ty,tz)
+        #   where c=cos(alpha) is the cosine of the angle alpha between any pair
+        #   of crystallographic vectors, tc, ty, tz are defined as
+        # tx=sqrt((1-c)/2), ty=sqrt((1-c)/6), tz=sqrt((1+2c)
+        tx = sqrt((1.0 - cos_alpha)/2.0)
+        ty = sqrt((1.0 - cos_alpha)/6.0)
+        tz = sqrt((1.0 + 2*cos_alpha)/3.0)
+        cell = aa * np.array([[ tx,   -ty,      tz], 
+                              [ 0.0,   2.0*ty,  tz], 
+                              [-tx,   -ty,      tz]])
+    elif ibrav == 6:
+        # Tetragonal P (st), simple tetragonal (p)
+        # v1 = a(1,0,0),  v2 = a(0,1,0),  v3 = a(0,0,c/a)
+        cell = aa * np.array([[1,  0,  0], 
+                              [0,  1,  0], 
+                              [0,  0,  cc/aa]])
+    elif ibrav == 7:
+        # Tetragonal I (bct), body centered tetragonal (i)
+        # v1 = (a/2)(1,-1,c/a),  v2 = (a/2)(1,1,c/a),  v3 = (a/2)(-1,-1,c/a)
+        cell = 0.5*aa * np.array([[ 1,  -1,  cc/aa], 
+                                  [ 1,   1,  cc/aa], 
+                                  [-1,  -1,  cc/aa]])        
+    elif ibrav == 8:
+        # Orthorhombic P, simple orthorhombic (p)
+        # v1 = (a,0,0),  v2 = (0,b,0), v3 = (0,0,c)
+        cell = np.array([[aa,  0,  0], 
+                         [0,   bb, 0], 
+                         [0,   0,  cc]])
+    elif ibrav == 9:
+        # Orthorhombic base-centered(bco), bco base centered orthorhombic
+        # v1 = (a/2,b/2,0),  v2 = (-a/2,b/2,0),  v3 = (0,0,c)
+        cell = np.array([[ aa/2.0,   bb/2.0,   0], 
+                         [-aa/2.0,   bb/2.0,   0], 
+                         [ 0,        0,        cc]])
+    elif ibrav == 10:
+        # Orthorhombic face-centered, face centered orthorhombic
+        # v1 = (a/2,0,c/2),  v2 = (a/2,b/2,0),  v3 = (0,b/2,c/2)
+        cell = np.array([[aa/2.0,    0,       cc/2.0], 
+                         [aa/2.0,    bb/2.0,  0], 
+                         [0,         bb/2.0,  cc/2.0]])
+    elif ibrav == 11:
+        # Orthorhombic body-centered, body centered orthorhombic
+        # v1 = (a/2,b/2,c/2),  v2 = (-a/2,b/2,c/2),  v3 = (-a/2,-b/2,c/2)
+        cell = np.array([[ aa/2.0,   bb/2.0,  cc/2.0], 
+                         [-aa/2.0,   bb/2.0,  cc/2.0],
+                         [-aa/2.0,  -bb/2.0,  cc/2.0]])
+    elif ibrav == 12:
+        # Monoclinic P, monoclinic (p)
+        # v1 = (a,0,0), v2= (b*cos(gamma), b*sin(gamma), 0),  v3 = (0, 0, c)
+        cell = np.array([[aa,             0,              0], 
+                         [bb*cos_gamma,  bb*sin_gamma,  0],
+                         [0,              0,              cc]])
+    elif ibrav == 13:
+        # Monoclinic base-centered, base centered monoclinic
+        # v1 = (a/2,0,-c/2), v2 = (b*cos(gamma),b*sin(gamma), 0), v3 = (a/2,0,c/2)
+        cell = np.array([[aa/2.0,         0,             -cc/2.0], 
+                         [bb*cos_gamma,   bb*sin_gamma,   0],
+                         [aa/2.0,         0,              cc/2.0]])
+    elif ibrav == 14:
+        # Triclinic
+        # v1 = (a, 0, 0),
+        # v2 = (b*cos(gamma), b*sin(gamma), 0)
+        # v3 = (c*cos(beta),  c*(cos(alpha)-cos(beta)cos(gamma))/sin(gamma),
+        # c*sqrt( 1 + 2*cos(alpha)cos(beta)cos(gamma)
+        #           - cos(alpha)^2-cos(beta)^2-cos(gamma)^2 )/sin(gamma) 
+        v1 = np.array([aa,0,0])
+        v2 = np.array([bb*cos_gamma, bb*sin_gamma, 0])
+        v3 = np.array([cc*cos_beta, 
+                       cc*(cos_alpha - cos_beta*cos_gamma)/sin_gamma, 
+                       cc*sqrt( 1 + 2*cos_alpha*cos_beta*cos_gamma - \
+                       cos_alpha**2.0-cos_beta**2.0-cos_gamma**2.0)/sin_gamma])
+        cell = np.array([v1, v2, v3])
+    else:
+        raise StandardError("illegal ibrav: %s" %ibrav)
+    return cell        
---
+++ test/test_ibrav.py
@@ -0,0 +1,76 @@
+import numpy as np
+from math import pi, cos
+from pwtools.pwscf import ibrav2cell
+from pwtools import crys
+
+def test():
+    # bogus
+    aa = 3.0
+    bb = 4.0
+    cc = 5.0
+    alpha = 66*pi/180
+    beta = 77*pi/180
+    gamma = 88*pi/180
+
+    ibrav = 1
+    celldm = [aa] + [None]*5
+    ibrav2cell(ibrav, celldm)
+    
+    ibrav = 2
+    celldm = [aa] + [None]*5
+    ibrav2cell(ibrav, celldm)
+    
+    ibrav = 3
+    celldm = [aa] + [None]*5
+    ibrav2cell(ibrav, celldm)
+    
+    ibrav = 4
+    celldm = [aa, None, cc/aa, None, None, None]
+    ibrav2cell(ibrav, celldm)
+    
+    ibrav = 5
+    celldm = [aa, None, None, cos(alpha), None, None]
+    ibrav2cell(ibrav, celldm)
+    
+    ibrav = 6
+    celldm = [aa, None, cc/aa, None, None, None]
+    ibrav2cell(ibrav, celldm)
+    
+    ibrav = 7
+    celldm = [aa, None, cc/aa, None, None, None]
+    ibrav2cell(ibrav, celldm)
+    
+    ibrav = 8
+    celldm = [aa, bb/aa, cc/aa, None, None, None]
+    ibrav2cell(ibrav, celldm)
+    
+    ibrav = 9
+    celldm = [aa, bb/aa, cc/aa, None, None, None]
+    ibrav2cell(ibrav, celldm)
+    
+    ibrav = 10
+    celldm = [aa, bb/aa, cc/aa, None, None, None]
+    ibrav2cell(ibrav, celldm)
+    
+    ibrav = 11
+    celldm = [aa, bb/aa, cc/aa, None, None, None]
+    ibrav2cell(ibrav, celldm)
+    
+    # celldm(4)=cos(ab) in doc!?
+    ibrav = 12 
+##    celldm = [aa, bb/aa, cc/aa, cos(alpha), None, None]
+    celldm = [aa, bb/aa, cc/aa, None, None, cos(gamma)]
+    ibrav2cell(ibrav, celldm)
+    
+    # celldm(4)=cos(ab) in doc!?
+    ibrav = 13
+##    celldm = [aa, bb/aa, cc/aa, cos(alpha), None, None]
+    celldm = [aa, bb/aa, cc/aa, None, None, cos(gamma)]
+    ibrav2cell(ibrav, celldm)
+    
+    # WOHOO!
+    ibrav = 14
+    celldm = [aa, bb/aa, cc/aa, cos(alpha), cos(beta), cos(gamma)]
+    cell=ibrav2cell(ibrav, celldm)
+    np.testing.assert_array_almost_equal(cell, 
+                                         crys.cc2cell(crys.celldm2cc(celldm)))
