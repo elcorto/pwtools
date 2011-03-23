db275011ad3c821c05c90867e8fb2c290589f26c
Steve Schmerler <mefx@gmx.net>
2011-03-23 18:22:04 +0100

    API: Rename bin/cif2pwin.py -> bin/cif2any.py and add output for Abinit. API: abinit.typat() is gone. Now, we have a class abinit.AbinitInput .

--- abinit.py
+++ abinit.py
@@ -2,23 +2,61 @@
 #
 # Tools for building Abinit input files.
 
-def get_typat(symbols, order):
-    """
-    args:
-    -----
-    symbols : sequence of strings
-        Atom symbols
-    order : dict
-        dict mapping atom symbols to atom number on the order of the
-        pseudopotentials in the files file
-    
-    returns:
-    --------
-    list of ints
+import numpy as np
+from pwtools import periodic_table
 
-    example:
-    --------
-    >>> get_typat(['Al']*3 + ['N']*2, {'Al':1, 'N':2})
-    [1,1,1,2,2]
-    """
-    return [order[ss] for ss in symbols]
+class AbinitInput(object):
+    """Class which builds some useful input variables based on `symbols`
+    alone."""
+    def __init__(self, symbols):
+        # XXX order of getters is important, use FelxibleGetters as base class
+        # if you extend this
+         
+        # ['N', 'Al', 'Al', 'Al', 'N', 'N', 'Al']]
+        self.symbols = symbols
+        # ['Al', 'N']
+        self.symbols_unique = self.get_symbols_unique()
+        # {'Al': 1, 'N': 2}
+        self.order = self.get_order()
+        # [2,1,1,1,2,2,1]
+        self.typat = self.get_typat()
+        # [13,7]
+        self.znucl = self.get_znucl()
+        # 2
+        self.ntypat = self.get_ntypat()
+
+    def get_symbols_unique(self):
+        return np.unique(self.symbols).tolist()
+
+    def get_order(self):
+        return dict([(sym, num+1) for num, sym in
+                     enumerate(self.symbols_unique)])
+
+    def get_typat(self):
+        """
+        returns:
+        --------
+        list of ints
+
+        example:
+        --------
+        >>> typat(['Al']*3 + ['N']*2, {'Al':1, 'N':2})
+        [1,1,1,2,2]
+        """
+        return [self.order[ss] for ss in self.symbols]
+    
+    def get_znucl(self):
+        """
+        returns:
+        --------
+        list of ints
+
+        example:
+        --------
+        >>> znucl({'Al':1, 'N':2})
+        [13,7]
+        """
+        return [periodic_table.pt[sym]['number'] for sym in self.symbols_unique]
+
+    def get_ntypat(self):
+        return len(self.order.keys())
---
+++ bin/cif2any.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# 
+# cif2pwin.py
+#
+# Extract information from a .cif file and print them in a format suitable for
+# inclusion in a pwscf and abinit input file.
+#
+# usage:
+#   cif2pwin.py foo.cif
+
+
+import sys
+import numpy as np
+
+from pwtools import parse, crys, periodic_table, pwscf, abinit
+from pwtools.common import str_arr, seq2str
+from pwtools import constants
+
+fn = sys.argv[1]
+pp = parse.CifFile(fn)
+pp.parse()
+cellr = crys.recip_cell(pp.cell)
+norms = np.sqrt((cellr**2.0).sum(axis=1))
+bar = '-'*78
+cryst_const = pp.cryst_const.copy()
+# in Bohr
+cryst_const[:3] /= constants.a0_to_A
+abiin = abinit.AbinitInput(pp.symbols)
+mass_unique = [periodic_table.pt[sym]['mass'] for sym in abiin.symbols_unique]                 
+atspec = pwscf.atspec_str(abiin.symbols_unique, 
+                          mass_unique, 
+                          [sym + '.UPF' for sym in abiin.symbols_unique])
+
+print bar
+print("""NOTE: celldm is valid only for ibrav=0 (pwscf)! The .cif file is
+assumed to contain NO symmetry information linking primitive and conventional
+cell!""")
+print bar
+
+print bar
+print "PWSCF"
+print bar
+
+print "celldm (a[Bohr], b/a, c/a, cos(bc), cos(ac), cos(ab):\n%s\n" %str_arr(pp.celldm)
+for ii, cd in enumerate(pp.celldm):
+    print "celldm(%i) = %.16e" %(ii+1, cd)
+print "ibrav = 0"
+print "nat = %i" %pp.natoms    
+print "ntyp = %i" %abiin.ntypat
+print "CELL_PARAMETERS alat\n%s" %str_arr(pp.cell / pp.cryst_const[0])
+print "ATOMIC_SPECIES\n%s" %atspec
+print "ATOMIC_POSITIONS crystal\n%s" %pp.atpos_str
+
+print bar
+print "ABINIT"
+print bar
+print "natom %i" %pp.natoms    
+print "ntypat %i" %abiin.ntypat
+print "typat %s" %seq2str(abiin.typat)
+print "znucl %s" %seq2str(abiin.znucl)
+print "acell %s" %str_arr(cryst_const[:3])
+print "angdeg %s" %str_arr(cryst_const[3:])
+print "xred\n%s" %str_arr(pp.coords)
+print "pseudopotential order:\n%s" %"\n".join(["%s %i" %(sym, zz) for sym,zz in 
+                                               zip(abiin.symbols_unique,
+                                                   abiin.znucl)])
+
+print bar
+print "GENERAL"
+print bar
+print "recip. cell:\n%s" %str_arr(cellr)
+print "relation of recip. vector lengths (a:b:c):\n%s" %str_arr(norms/norms.min())
