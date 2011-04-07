544092de26df0e4a25189c1757a094180b14c336
Steve Schmerler <mefx@gmx.net>
2011-04-07 16:00:35 +0200

    ENH: Add consistency check to test/test_qha.py .

--- test/test_qha.py
+++ test/test_qha.py
@@ -2,13 +2,15 @@
 #
 # Test thermo.HarmonicThermo against results from F_QHA.f90 from QE 4.2 .
 
-def test():
-    import numpy as np
-    from pwtools.thermo import HarmonicThermo
-    from pwtools import common
-    
-    # Verify result tests
+import numpy as np
+from pwtools.thermo import HarmonicThermo
+from pwtools import common
+from pwtools.constants import Ry_to_J
 
+def assrt_aae(*args, **kwargs):
+    np.testing.assert_array_almost_equal(*args, **kwargs)
+
+def test():
     def pack(fns):
         for fn in fns:
             common.system('gzip %s' %fn)
@@ -29,20 +31,22 @@ def test():
 
     fqha = np.loadtxt(fqha_fn)
     pdos = np.loadtxt(pdos_fn)
-    T = fqha[:,0] 
+    temp = fqha[:,0] 
 
-    #
+    #--------------------------------------------------------------------
+    # Verify against ref data
+    #--------------------------------------------------------------------
+    
     # No nan warnings, b/c we do not take all data into account. However
     # assert_array_almost_equal() will fail with decimal=3.
     # 
-    ##ha = HarmonicThermo(pdos[3:,0], pdos[3:,1], T, fixzero=True, fixnan=True,
+    ##ha = HarmonicThermo(pdos[3:,0], pdos[3:,1], temp, fixzero=True, fixnan=True,
     ##                    checknan=True)
 
-    #
+    
     # all data, mmaybe get some nans, but assert_array_almost_equal() will pass
     # with decimal=3 .
-    #
-    ha = HarmonicThermo(pdos[:,0], pdos[:,1], T, fixzero=True, fixnan=True,
+    ha = HarmonicThermo(pdos[:,0], pdos[:,1], temp, fixzero=True, fixnan=True,
                         checknan=True)
 
     dct = {'evib': Store(arr1=ha.evib(), arr2=fqha[:,1]),
@@ -51,25 +55,34 @@ def test():
            'svib': Store(arr1=ha.svib(), arr2=fqha[:,4]),
            }
     for key, store in dct.iteritems():
-        np.testing.assert_array_almost_equal(store.arr1, store.arr2, decimal=2)
+        assrt_aae(store.arr1, store.arr2, decimal=2)
     ##from matplotlib import pyplot as plt        
     ##    plt.figure()
-    ##    plt.plot(T, store.arr1, label='%s: ha'%key)
-    ##    plt.plot(T, store.arr2, label='%s: fqha'%key)
-    ##    plt.plot(T, store.arr1 - store.arr2, label='%s: diff'%key)
+    ##    plt.plot(temp, store.arr1, label='%s: ha'%key)
+    ##    plt.plot(temp, store.arr2, label='%s: fqha'%key)
+    ##    plt.plot(temp, store.arr1 - store.arr2, label='%s: diff'%key)
     ##    plt.legend()
     ##plt.show()
     
-
+    #--------------------------------------------------------------------
+    # Consistency
+    #--------------------------------------------------------------------
+    
+    # Fvib = Evib -T*Svib
+    assrt_aae(dct['fvib'].arr1*Ry_to_J, 
+              (dct['evib'].arr1 - temp*dct['svib'].arr1)*Ry_to_J)
+    
+    #--------------------------------------------------------------------
     # API tests
+    #--------------------------------------------------------------------
 
     # use temp arg in methods
     ha = HarmonicThermo(pdos[:,0], pdos[:,1], fixzero=True, fixnan=True,
                         checknan=True)
-    x=ha.evib(T)                        
-    x=ha.fvib(T)
-    x=ha.cv(T)
-    x=ha.svib(T)
+    x=ha.evib(temp)                        
+    x=ha.fvib(temp)
+    x=ha.cv(temp)
+    x=ha.svib(temp)
     pack(files)
     
     # test fix* 
