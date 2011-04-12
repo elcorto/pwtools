eea7dc65971a5a3e7fde1b1dd273f8c43fe45dc3
Steve Schmerler <mefx@gmx.net>
2011-04-12 12:59:21 +0200

    ENH: abinit.AbinitInput: Add method print_input(). Enhance docstring.

--- abinit.py
+++ abinit.py
@@ -4,10 +4,21 @@
 
 import numpy as np
 from pwtools import periodic_table
+from pwtools.common import seq2str
 
 class AbinitInput(object):
     """Class which builds some useful input variables based on `symbols`
-    alone."""
+    alone.
+    
+    example:
+    --------
+    >>> aa = AbinitInput(['Al']*5 + ['N']*5 + ['O']*2 + ['N'])
+    >>> aa.print_input()
+    natom 13
+    ntypat 3
+    typat 1 1 1 1 1 2 2 2 2 2 3 3 2
+    znucl 13 7 8
+    """
     def __init__(self, symbols):
         # XXX order of getters is important, use FelxibleGetters as base class
         # if you extend this
@@ -40,7 +51,7 @@ class AbinitInput(object):
 
         example:
         --------
-        >>> typat(['Al']*3 + ['N']*2, {'Al':1, 'N':2})
+        >>> get_typat(['Al']*3 + ['N']*2, {'Al':1, 'N':2})
         [1,1,1,2,2]
         """
         return [self.order[ss] for ss in self.symbols]
@@ -53,10 +64,17 @@ class AbinitInput(object):
 
         example:
         --------
-        >>> znucl({'Al':1, 'N':2})
+        >>> get_znucl({'Al':1, 'N':2})
         [13,7]
         """
         return [periodic_table.pt[sym]['number'] for sym in self.symbols_unique]
 
     def get_ntypat(self):
         return len(self.order.keys())
+    
+    def print_input(self):
+        print "natom %i" %len(self.symbols)    
+        print "ntypat %i" %self.ntypat
+        print "typat %s" %seq2str(self.typat)
+        print "znucl %s" %seq2str(self.znucl)
+
