96114e811ec8dd22727caeba4ba504719c6e7092
Steve Schmerler <mefx@gmx.net>
2011-04-12 13:00:20 +0200

    ENH: Update mttk.py docstring.

--- mttk.py
+++ mttk.py
@@ -3,11 +3,14 @@
 # Fictitious masses for Nose-Hoover thermostat chains (NHCs) and barostat from
 # the MTTK paper [1].
 #
-# Atomic units: All "masses" have dimension [mass * length**2] = [kg * m**2] and
-# are converted to Hartree atomic units: mass[kg] / m0 (electron mass),
-# length[m] / a0 (Bohr). We are not totally sure, but we belief that these area
-# the units for qmass an bmass used in Abinit. As usual, this is not documented
-# anywhere. 
+# Atomic units: All fictitious masses have dimension [mass * length**2] = [kg *
+# m**2] and are converted to Hartree atomic units: mass[kg] / m0 (electron
+# mass), length[m] / a0 (Bohr). We are not totally sure, but we belief that
+# these are the units for qmass an bmass used in Abinit. As usual, this is not
+# really documented anywhere, but we rely of the documentation for "noseinert"
+# (ionmov 8), which states that "... the inertia factor WT of the Nose-Hoover
+# thermostat (when ionmov=8), in atomic units of weight*length^2, that is
+# (electron mass)*(Bohr)^2".
 #
 # [1] Glenn J. Martyna and Mark E. Tuckerman and Douglas J. Tobias and Michael
 #     L. Klein, "Explicit reversible integrators for extended systems
