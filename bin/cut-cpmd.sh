4896f18de2e8070fee319fbb6891e9c4cfa8a062
Steve Schmerler <mefx@gmx.net>
2012-01-25 18:30:55 +0100

    ENH: Add bin/cut-cpmd.sh

---
+++ bin/cut-cpmd.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+prog=$(basename $0)
+simulate=false
+sed_opts="-r -e"
+inplace=true
+bakname=bak
+
+usage(){
+    cat << EOF
+
+Truncate CPMD output files from the <step>'s iteration. Which files is
+hardcoded:
+
+CELL
+ENERGIES
+STRESS
+TRAJECTORY
+FTRAJECTORY
+TRAJEC.xyz
+
+Files which don't exist will be skipped.
+
+usage:
+------
+$prog [-hs] <dir> <step>
+
+options:
+--------
+-s : simulate
+--not-inplace : don't use ``sed -i.$bakname ...``, use if YKWYAD 
+
+example
+-------
+Say your calc ran 8423 steps, then was killed. The RESTART is written every 50
+steps, so the last was written at 8400. A restart with fresh files in a new
+directory using "RESTART ACCUMULATORS" and continuing files with <<< NEW
+DATA >>> starts at 8401 and thus repeats 8401..8423. You want to delete 23
+steps from the old CELL, ENERGIES, etc files before the restart. Then use:
+
+$ $prog /path/to/calc/ 8401 
+EOF
+}
+
+msg(){
+    echo "$prog: $@"
+}    
+
+err(){
+    echo "$prog: error: $@"
+    exit 1
+}    
+
+cmdline=$(getopt -o hs -l not-inplace -- "$@")
+eval set -- "$cmdline"
+while [ $# -gt 0 ]; do
+    case "$1" in 
+        -s)
+            simulate=true
+            ;;
+        --not-inplace)
+            inplace=false
+            ;;
+        -h)
+            usage
+            exit 0
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "cmdline error"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+[ $# -ne 2 ] && err "illegal number of args (need 2)"
+dr=$1
+[ -d "$dr" ] || err "not a dir: $dr"
+step=$2
+
+while read conf; do
+    fn=$(echo "$conf" | awk -F '@@' '{print $1}')
+    sedstr=$(echo "$conf" | awk -F '@@' '{print $2}')
+    _bakname=$(echo "$conf" | awk -F '@@' '{print $3}')
+    [ -n "$_bakname" ] && bakname=$_bakname
+    if $inplace; then
+        cmd="sed -i.$bakname $sed_opts '$sedstr' $fn"
+    else        
+        cmd="sed $sed_opts '$sedstr' $fn"
+    fi        
+    if [ -f $fn ]; then
+        echo ">> $fn <<"
+        echo "  $cmd"
+        $simulate || eval $cmd
+    else
+        msg "not found: $fn"
+    fi        
+done << EOF
+$dr/CELL@@/.*CELL PARAMETERS.*Step.*$step.*/,$ d
+$dr/ENERGIES@@/^\\\s+$step\\\s.*/,$ d
+$dr/STRESS@@/.*TOTAL STRESS.*Step.*$step.*/,$ d
+$dr/TRAJECTORY@@/^\\\s+$step\\\s.*/,$ d
+$dr/FTRAJECTORY@@/^\\\s+$step\\\s.*/,$ d
+$dr/TRAJEC.xyz@@/^\\\s+STEP:\\\s+$step.*/,$ d
+$dr/TRAJEC.xyz@@$,$ d@@bak2
+EOF
---
+++ test/test_cut_cpmd.py
@@ -0,0 +1,19 @@
+import os.path
+from pwtools.parse import CpmdMDOutputFile
+from pwtools import common
+from testenv import testdir
+
+def run(dr, none_attrs=[]):
+    # dr       = 'files/cpmd/md_bo'
+    # basedr   = 'files/cpmd'
+    # archive  = 'files/cpmd/md_bo.tgz'
+    if dr.strip().endswith('/'):
+        dr = dr.strip()[:-1]
+    basedr = os.path.dirname(dr)
+    common.system('tar -C %s -xzf %s.tgz' %(basedr, dr))
+    common.system('../bin/cut-cpmd.sh %s 20 > %s/cut-cpmd.log' %(dr, testdir))
+    common.system('rm -r %s' %dr)
+
+def test():
+    run(dr='files/cpmd/md_cp_pr/')
+
