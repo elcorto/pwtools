70f76408a93a6d11e7143d324138671fc0b6df85
Steve Schmerler <mefx@gmx.net>
2013-01-09 08:20:23 +0100

    ENH: Pimp test/test_pdb.py .

--- test/test_pdb.py
+++ test/test_pdb.py
@@ -1,7 +1,8 @@
-def test():
-    from pwtools.parse import PDBFile
-    from pwtools import common
+import numpy as np
+from pwtools.parse import PDBFile
+from pwtools import common
 
+def test_pdb():
     struct = PDBFile('files/pdb_struct.pdb', 
                      units={'length':    1.0}).get_struct()
      
@@ -10,3 +11,17 @@ def test():
     assert struct.symbols is not None
     assert struct.coords is not None
     assert struct.coords_frac is not None    
+
+    assert struct.symbols == ['C' ,'O' ,'O' ,'Na','H' ,'H' ,'O' ,'O']
+    coords = np.array(\
+        [[5.759,   5.581,   5.339], 
+         [5.759,   6.951,   5.339],
+         [6.980,   4.876,   5.339],
+         [7.406,   6.575,   5.339],
+         [5.701,   2.442,   7.733],
+         [5.908,   0.887,   7.280],
+         [6.008,   1.840,   6.996],
+         [2.880,   7.979,   2.600]])
+    cryst_const = np.array([10.678, 10.678,10.678,90.00,90.00, 90.00])   
+    assert np.allclose(coords, struct.coords)             
+    assert np.allclose(cryst_const, struct.cryst_const)             
