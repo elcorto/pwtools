76048912346528189f6732ea635ea323a151c9d2
Steve Schmerler <mefx@gmx.net>
2013-01-10 19:12:29 +0100

    ENH: test/tools.py: Add assert_all_types_equal(). This is supposed to test      *all* Python types.

--- test/tools.py
+++ test/tools.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+arr_t = type(np.array([1.0]))
+dict_t = type({'1': 1})
+
 class DictWithArraysFactory(object):
     """Factory for creating functions with compare dicts with numpy arrays as
     values."""
@@ -7,7 +10,6 @@ class DictWithArraysFactory(object):
         self.array_comp_func = array_comp_func
     
     def __call__(self, d1, d2, attr_lst=None):
-        arr_t = type(np.array([1.0]))
         attr_lst = d1.keys() if attr_lst is None else attr_lst
         for key, val in d1.iteritems():
             if key in attr_lst:
@@ -26,7 +28,7 @@ def assert_attrs_not_none(pp, attr_lst=None, none_attrs=[]):
             assert attr is not None, "FAILED: obj: %s attr: %s is None" \
                 %(str(pp), name)
 
-
+      
 aaae = np.testing.assert_array_almost_equal
 aae = np.testing.assert_almost_equal
 
@@ -36,4 +38,11 @@ assert_dict_with_arrays_equal = DictWithArraysFactory(lambda x,y: (x==y).all())
 adae = assert_dict_with_arrays_almost_equal
 ade = assert_dict_with_arrays_equal
 
+def assert_all_types_equal(aa,bb):
+    if type(aa) == arr_t:
+       assert (aa == bb).all()
+    elif type(aa) == dict_t:   
+        assert_dict_with_arrays_equal(aa,bb)
+    else:
+        assert aa == bb
 
