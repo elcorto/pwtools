cd6a62299bedecad074f1d3cd4ff20c160ec2889
Steve Schmerler <mefx@gmx.net>
2014-10-24 21:12:47 +0200

    API: Return 3-tuple fig,ax,axdos instead of fig,ax. ENH: kpath.plot_dis(): Add kwds show_coords, dos and ylim.

--- kpath.py
+++ kpath.py
@@ -80,7 +80,7 @@ class SpecialPointsPath(object):
 
 
 def plot_dis(path_norm, freqs, special_points_path=None,
-             ax=None, **kwargs):
+             show_coords=None, dos=None, ax=None, ylim=None, **kwargs):
     """Plot dispersion. 
     
     See ``bin/plot_dispersion.py`` for a usage example. This lives here (and not in
@@ -89,6 +89,12 @@ def plot_dis(path_norm, freqs, special_points_path=None,
     
     See :func:`~pwtools.pwscf.read_matdyn_freq` for how to get `freqs` in the
     case of phonon dispersions.
+
+    This function is a convenience function, which can even plot the DOS as
+    well. We do not expose many matplotlib parameters here. If you want to tweak
+    your plot much, then operate on the returned `fig`, `ax` (dispersion) and
+    `axdos` (dos), or copy and hack the function, which might be actually the easiest
+    way.
     
     Parameters
     ----------
@@ -99,26 +105,45 @@ def plot_dis(path_norm, freqs, special_points_path=None,
         `nbnd` frequencies for each band at each k-point
     special_points_path : optional, :class:`SpecialPointsPath` instance
         used for pretty-printing the x-axis (set special point labels)
+    show_coords : 'cart', 'frac', None
+        Show the cartesian or fractional coordinates of special points in the
+        x-axis label, on neither if None.
+    dos : array (N,2) or None
+        array with phonon dos to plot: ``dos[:,0]=freq``, ``dos[:,1]=phdos``
     ax : matplotlib AxesSubplot (e.g. from ``fig,ax=pwtools.mpl.fig_ax()``)
+        automatically created if None
+    ylim : tuple (2,)
+        frequency axis limits
     **kwargs : keywords
         passed to plot()
     
+    Returns
+    -------
+    fig, ax, axdos
+    fig : matplotlib Figure to ax
+    ax : matplotlib AxesSubplot with dispersion
+    axdos : matplotlib AxesSubplot with dos, or None if `dos=None` 
+
     Examples
     --------
-    >>> spp = kpath.SpecialPointsPath(ks=np.array([[0,0,0], [.5,0,0], [.7,0,0]]),
+    >>> spp = kpath.SpecialPointsPath(ks=np.array([[0,0,0], [1.5,0,0], [2.3,0,0]]),
                                       symbols=['A', 'B', 'C'])
-    >>> path_norm = np.linspace(0,1,100)
-    >>> freqs = np.random.rand(100,5) 
-    >>> # create fig,ax inside
-    >>> fig1,ax1 = kpath.plot_dis(path_norm, freqs, spp)
-    >>> # pass ax from outside, returns fig2,ax2 but we don't use that b/c ax2
+    >>> path_norm = np.linspace(0,2.5,100)
+    >>> freqs = np.random.rand(100,5)*500 
+    >>> # create fig,ax inside, returned axdos=None
+    >>> fig,ax,axdos = kpath.plot_dis(path_norm, freqs, spp)
+    >>> # pass ax from outside, returns fig,ax but we don't use that b/c ax
     >>> # is in-place modified
-    >>> fig2,ax2 = mpl.fig_ax()
-    >>> kpath.plot_dis(path_norm, freqs, spp, ax=ax2)
+    >>> fig,ax = mpl.fig_ax()
+    >>> kpath.plot_dis(path_norm, freqs, spp, ax=ax)
+    >>> # plot also DOS
+    >>> dos = np.empty((30,2)); dos[:,0]=np.linspace(0,500,30); dos[:,1]=rand(30)
+    >>> fig,ax,axdos = kpath.plot_dis(path_norm, freqs, spp, dos=dos)
 
     See Also
     --------
     :func:`get_path_norm`
+    :func:`pwtools.pwscf.read_matdyn_freq`
     :ref:`dispersion_example`
     """
     if ax is None:
@@ -128,19 +153,39 @@ def plot_dis(path_norm, freqs, special_points_path=None,
     # Plot columns of `freq` against q points (path_norm)
     ax.plot(path_norm, freqs, **kwargs)
     if special_points_path is not None:
-        yl = ax.get_ylim()
+        ylim = ax.get_ylim() if ylim is None else ylim
         ks, ks_frac, nrm, symbols = \
             special_points_path.ks, \
             special_points_path.ks_frac, \
             special_points_path.path_norm, \
             special_points_path.symbols
-        ax.vlines(nrm, yl[0], yl[1])
+        ax.vlines(nrm, ylim[0], ylim[1])
         fmtfunc = lambda x: "%.2g" %x
-        ks_plot = ks if ks_frac is None else ks_frac
-        labels = ['%s\n[%s]' %(sym, common.seq2str(kk, func=fmtfunc,sep=','))\
-                  for sym,kk in zip(symbols, ks_plot)]
+        if show_coords is None:
+            labels = symbols
+        else:            
+            if show_coords == 'cart':
+                ks_plot = ks 
+            elif show_coords == 'frac':
+                ks_plot = ks_frac
+            else:
+                raise StandardError("show_coords = 'cart', 'frac' or "
+                                    "None needed")
+            labels = ['%s\n[%s]' %(sym, common.seq2str(kk, func=fmtfunc,sep=','))\
+                      for sym,kk in zip(symbols, ks_plot)]
         ax.set_xticks(nrm)
         ax.set_xticklabels(labels)
         ax.set_xlim(path_norm[0], path_norm[-1])
         ax.set_ylabel("frequency (cm$^{-1}$)")
-    return fig,ax        
+        if dos is not None:
+            # http://matplotlib.org/examples/axes_grid/scatter_hist.html
+            from mpl_toolkits.axes_grid1 import make_axes_locatable
+            divider = make_axes_locatable(ax)
+            axdos = divider.append_axes("right", 1.2, pad=0.1, sharey=ax)
+            axdos.plot(dos[:,1], dos[:,0], 'b')
+            mpl.plt.setp(axdos.get_yticklabels(), visible=False)
+            mpl.plt.setp(axdos.get_xticklabels(), visible=False)
+            axdos.xaxis.set_tick_params(size=0)
+        else:
+            axdos = None
+    return fig, ax, axdos        
--- test/test_kpath.py
+++ test/test_kpath.py
@@ -9,18 +9,18 @@ def test_kpath():
 
 def test_plot_dis():
     spp = kpath.SpecialPointsPath(ks=np.array([[0,0,0], [.5,0,0], [.7,0,0]]),
+                                  ks_frac=np.array([[0,0,0], [.55,0,0], [.77,0,0]]),  
                                   symbols=['A','B', 'C'])
     path_norm = np.linspace(0,1,100)
     nfreq = 5
     freqs = np.random.rand(100, nfreq) 
     try:
         print os.environ['DISPLAY']
-        # create fig,ax inside
-        fig1,ax1 = kpath.plot_dis(path_norm, freqs, spp)
-        # pass ax from outside, returns fig2,ax2 but we don't use that b/c ax
-        # is in-place modified
+        fig1,ax1,axdos1 = kpath.plot_dis(path_norm, freqs, spp,
+                                         show_coords='cart')
+        assert axdos1 is None
         fig2,ax2 = mpl.fig_ax()
-        kpath.plot_dis(path_norm, freqs, spp, ax=ax2)
+        kpath.plot_dis(path_norm, freqs, spp, ax=ax2, show_coords='frac')
         lines1 = ax1.get_lines()
         lines2 = ax2.get_lines()
         for idx in range(nfreq):
@@ -30,6 +30,13 @@ def test_plot_dis():
             y2 = lines2[idx].get_ydata()
             assert (x1 == x2).all()
             assert (y1 == y2).all()
+        faxis = np.linspace(freqs.min(), freqs.max(), 30)                
+        dos = np.array([faxis, np.random.rand(len(faxis))]).T            
+        fig3,ax3,ax3dos = kpath.plot_dis(path_norm, freqs, spp, dos=dos,
+                                         show_coords=None)
+        # plot 90 rotated -> x and y swapped
+        assert (ax3dos.get_lines()[0].get_xdata() == dos[:,1]).all()
+        assert (ax3dos.get_lines()[0].get_ydata() == dos[:,0]).all()
     except KeyError:
         warnings.warn("no DISPLAY environment variable, skipping test")
 
