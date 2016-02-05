6f0765439d1d21bfb9bd2870a54ae8a6c6a663de
Steve Schmerler <mefx@gmx.net>
2016-02-05 16:19:39 +0100

    BUG: git transition: delete version.py and its test
    It uses .hgtags which we don't have anymore.

--- test/test_version.py
+++
@@ -1,8 +0,0 @@
-
-
-def test_version():
-    from pwtools import version
-    from distutils.version import StrictVersion
-
-    assert version.version_str != '0.0.0'
-    assert version.version != StrictVersion('0.0.0')
