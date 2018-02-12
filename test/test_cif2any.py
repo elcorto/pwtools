342ef36fdfea6c1f9f778f8f9e5b659453dabb64
Steve Schmerler <git@elcorto.com>
2018-02-12 16:57:15 +0100

    PY3: cif cmd line test: discard warnings on stderr
    This makes tests using common.backtick() fail b/c that raises an
    exception if stderr is not empty.

--- test/test_cif2any.py
+++ test/test_cif2any.py
@@ -3,4 +3,4 @@ from pwtools.test import tools
 
 def test_cif2any():
     tools.skip_if_pkg_missing('CifFile')
-    print(common.backtick("../bin/cif2any.py files/cif_struct.cif"))
+    print(common.backtick("../bin/cif2any.py files/cif_struct.cif 2>/dev/null"))
--- test/test_cif2sgroup.py
+++ test/test_cif2sgroup.py
@@ -3,4 +3,4 @@ from pwtools.test import tools
 
 def test_cif2sgroup():
     tools.skip_if_pkg_missing('CifFile')
-    print(common.backtick("../bin/cif2sgroup.py files/cif_struct.cif"))
+    print(common.backtick("../bin/cif2sgroup.py files/cif_struct.cif 2>/dev/null"))
