4d79ecf01d0155edf064751547f05a5e1761e53d
Steve Schmerler <git@elcorto.com>
2018-02-18 01:51:21 +0100

    TST: make test_interpol.py more verbose
    Add a test class and individual methods for each test. Makes debugging
    easier.

--- pwtools/test/test_interpol.py
+++ pwtools/test/test_interpol.py
@@ -15,7 +15,7 @@ def dump(obj, testdir=testdir):
     pickle.dump(obj, fd, 2)
     fd.close()
 
-def get_test_data():
+def get_data():
     x = np.linspace(-5,5,20) 
     y = x 
     X,Y = np.meshgrid(x,y); X=X.T; Y=Y.T 
@@ -24,74 +24,79 @@ def get_test_data():
     tgt = np.array([  5.0 ,  30])
     return dd, tgt
 
-def test_interpol2d():
-    dd, tgt = get_test_data()
-
-    inter = num.Interpol2D(dd=dd, what='rbf_multi') 
-    assert np.allclose(inter([[-3,-4],[0,0]]), tgt)
-    assert np.allclose(return_min(inter), 5.0)
-    dump(inter)
+class TestInterpol2D:
+    def __init__(self):
+        self.dd, self.tgt = get_data()
     
-    inter = num.Interpol2D(dd=dd, what='rbf_inv_multi') 
-    assert np.allclose(inter([[-3,-4],[0,0]]), tgt)
-    assert np.allclose(return_min(inter), 5.0)
-    dump(inter)
+    def test_rbf_multi(self):
+        inter = num.Interpol2D(dd=self.dd, what='rbf_multi') 
+        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        assert np.allclose(return_min(inter), 5.0)
+        dump(inter)
     
-    inter = num.Interpol2D(dd=dd, what='rbf_gauss')
-    assert np.allclose(inter([[-3,-4],[0,0]]), tgt)
-    assert np.allclose(return_min(inter), 5.0, atol=1e-5)
-    dump(inter)
+    def test_rbf_inv_multi(self):
+        inter = num.Interpol2D(dd=self.dd, what='rbf_inv_multi') 
+        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        assert np.allclose(return_min(inter), 5.0)
+        dump(inter)
     
-    # pickling this fails:
-    #
-    #  ERROR: pwtools.test.test_interpol.test_interpol2d
-    #  PicklingError: Can't pickle <type 'function'>: attribute lookup __builtin__.function failed
-    #  FAILED (errors=1)
-    #
-    # The reason is that a function _call() inside __init__ is defined and
-    # pickle cannot serialize nested functions. But also defining a class-level
-    # function self._call_bispl instead doesn't work. So there is no solution
-    # here. You need to store the to-be-interpolated data and build the
-    # interpolator again as needed. Not a speed problem, just inconvenient. All
-    # other interpolator types can be pickled.
-    inter = num.Interpol2D(dd=dd, what='bispl')
-    assert np.allclose(inter([[-3,-4],[0,0]]), tgt)
-    assert np.allclose(return_min(inter), 5.0)
-    ##dump(inter)
-    
-    # linear, ct and nearest are very inaccurate, use only for plotting!
-    inter = num.Interpol2D(dd=dd, what='linear')
-    assert np.allclose(inter([[-3,-4],[0,0]]), tgt, atol=5e-1)
-    assert np.allclose(return_min(inter), 5.0, atol=1e-1)
-    dump(inter)
-    
-    # don't even test accuracy here
-    inter = num.Interpol2D(dd=dd, what='nearest')
-    dump(inter)
-    
-    # API
-    inter = num.Interpol2D(xx=dd.xx, yy=dd.yy, values=dd.zz, what='bispl')
-    assert np.allclose(inter([[-3,-4],[0,0]]), tgt)
-    assert np.allclose(return_min(inter), 5.0)
-
-    inter = num.Interpol2D(points=dd.XY, values=dd.zz, what='bispl')
-    assert np.allclose(inter([[-3,-4],[0,0]]), tgt)
-    assert np.allclose(return_min(inter), 5.0)
-
-    inter = num.Interpol2D(dd.XY, dd.zz, what='bispl')
-    assert np.allclose(inter([[-3,-4],[0,0]]), tgt)
-    assert np.allclose(return_min(inter), 5.0)
-
+    def test_rbf_gauss(self):
+        inter = num.Interpol2D(dd=self.dd, what='rbf_gauss')
+        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        assert np.allclose(return_min(inter), 5.0, atol=1e-5)
+        dump(inter)
    
-def test_interpol2d_CloughTocher():
-    dd, tgt = get_test_data()
-    try:
-        from scipy.interpolate import CloughTocher2DInterpolator
-        inter = num.Interpol2D(dd=dd, what='ct')
-        assert np.allclose(inter([[-3,-4],[0,0]]), tgt,
-            atol=1e-1)
+    def test_bispl(self):
+        # pickling this fails:
+        #
+        #  ERROR: pwtools.test.test_interpol.test_interpol2d
+        #  PicklingError: Can't pickle <type 'function'>: attribute lookup __builtin__.function failed
+        #  FAILED (errors=1)
+        #
+        # The reason is that a function _call() inside __init__ is defined and
+        # pickle cannot serialize nested functions. But also defining a class-level
+        # function self._call_bispl instead doesn't work. So there is no solution
+        # here. You need to store the to-be-interpolated data and build the
+        # interpolator again as needed. Not a speed problem, just inconvenient. All
+        # other interpolator types can be pickled.
+        inter = num.Interpol2D(dd=self.dd, what='bispl')
+        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        assert np.allclose(return_min(inter), 5.0)
+        ##dump(inter)
+    
+    def test_linear(self):
+        # linear, ct and nearest are very inaccurate, use only for plotting!
+        inter = num.Interpol2D(dd=self.dd, what='linear')
+        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt, atol=5e-1)
         assert np.allclose(return_min(inter), 5.0, atol=1e-1)
         dump(inter)
-    except ImportError:
-        tools.skip("couldn't import scipy.interpolate.CloughTocher2DInterpolator")
+    
+    def test_nearest(self):
+        # don't even test accuracy here
+        inter = num.Interpol2D(dd=self.dd, what='nearest')
+        dump(inter)
+    
+    def test_ct(self):
+        try:
+            from scipy.interpolate import CloughTocher2DInterpolator
+            inter = num.Interpol2D(dd=self.dd, what='ct')
+            assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt,
+                atol=1e-1)
+            assert np.allclose(return_min(inter), 5.0, atol=1e-1)
+            dump(inter)
+        except ImportError:
+            tools.skip("couldn't import scipy.interpolate.CloughTocher2DInterpolator")
+    
+    def test_api_bispl(self):
+        # API
+        inter = num.Interpol2D(xx=self.dd.xx, yy=self.dd.yy, values=self.dd.zz, what='bispl')
+        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        assert np.allclose(return_min(inter), 5.0)
 
+        inter = num.Interpol2D(points=self.dd.XY, values=self.dd.zz, what='bispl')
+        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        assert np.allclose(return_min(inter), 5.0)
+
+        inter = num.Interpol2D(self.dd.XY, self.dd.zz, what='bispl')
+        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        assert np.allclose(return_min(inter), 5.0)
