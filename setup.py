68fd9b1ce41fe1be121617d9173e68aa07747337
Steve Schmerler <git@elcorto.com>
2018-02-19 01:32:06 +0100

    ENH: setup.py: add simple way to build extensions

--- setup.py
+++ setup.py
@@ -44,17 +44,28 @@ for pip_name,import_name,op,ver in reqs:
 
 print("install_requires: {}".format(install_requires))
 
+# Instead of fighting with numpy.distutils (see
+# https://stackoverflow.com/a/41896134), we use our Makefile which we know
+# works well (OpenMP etc, see "make help") and copy the *.so files using the
+# package_data trick below. Makefile will copy the *.so files to pwtools/, so
+# we just need to tell setuptools that these are "data files" that we wish to
+# copy when installing, along with all *.py files.
+from subprocess import run
+run("cd src; make", shell=True, check=True)
+
 setup(
     name='pwtools',
-    version='0.9.9',
-    description='XXX',
+    version='0.9.0',
+    description='pre- and postprocessing of atomic calculations',
     long_description=long_description,
     url='https://github.com/elcorto/pwtools',
     author='Steve Schmerler',
     author_email='git@elcorto.com',
     license='BSD 3-Clause',
-    keywords='XXX',
+    keywords='ase scipy atoms simulation database postprocessing',
     packages=find_packages(),
     install_requires=install_requires,
+    python_requires='>=3',
+    package_data={'pwtools': ['*.so']},
 ##    scripts=['{}/{}'.format(bindir, script) for script in os.listdir(bindir)]
 )
