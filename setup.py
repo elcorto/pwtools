a47329e040fb029c3be5ccec1c522d773c6835ef
Steve Schmerler <git@elcorto.com>
2019-02-27 02:21:44 +0100

    BLD: rm pip hack in setup.py, add requirements.txt
    Use something like mkvirtualenv --system-site-packages instead.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # publish on pypi
 # ---------------
 #   $ python3 setup.py sdist
-#   $ twine upload dist/<this-package>-x.y.z.tar.gz 
+#   $ twine upload dist/<this-package>-x.y.z.tar.gz
 
 import os, importlib
 from setuptools import setup, find_packages
@@ -11,38 +11,6 @@ here = os.path.abspath(os.path.dirname(__file__))
 bindir = 'bin'
 with open(os.path.join(here, 'README.rst')) as fd:
     long_description = fd.read()
-
-# Hack to make pip respect system packages. 
-install_requires = []
-
-# (pip name, import name, operator, version)
-# ('numpy', 'numpy', '>', '1.0')
-reqs = [('matplotlib', 'matplotlib', None, None),
-        ('nose', 'nose', None, None),
-        ('numpy', 'numpy', None, None),
-        ('scipy', 'scipy', None, None),
-        ('h5py', 'h5py', None, None),
-        ('ase', 'ase', None, None),
-        ('numpydoc', 'numpydoc', None, None),
-        ('PyCifRW', 'CifFile', None, None),
-        ('pyspglib', 'pyspglib', None, None),
-##        ('spglib', 'spglib', None, None),
-        ]
-
-for pip_name,import_name,op,ver in reqs:
-    print("checking dependency: {}".format(import_name))
-    req = pip_name + op + ver if op and ver else pip_name
-    try:
-        pkg = importlib.import_module(import_name)
-        if op and ver:
-            cmd = "Version(pkg.__version__) {op} Version('{ver}')".format(op=op,
-                                                                          ver=ver)
-            if not eval(cmd):
-                install_requires.append(req)
-    except ImportError:
-        install_requires.append(req)
-
-print("install_requires: {}".format(install_requires))
 
 # Instead of fighting with numpy.distutils (see
 # https://stackoverflow.com/a/41896134), we use our Makefile which we know
@@ -64,7 +32,7 @@ setup(
     license='BSD 3-Clause',
     keywords='ase scipy atoms simulation database postprocessing',
     packages=find_packages(),
-    install_requires=install_requires,
+    install_requires=open('requirements.txt').read().splitlines(),
     python_requires='>=3',
     package_data={'pwtools': ['*.so']},
 ##    scripts=['{}/{}'.format(bindir, script) for script in os.listdir(bindir)]
