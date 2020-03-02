c32f4811d9a428ab16b7bae6b674180f6c30205b
Steve Schmerler <git@elcorto.com>
2020-03-02 14:05:50 +0100

    TST: move test_inner_points_mask() to test_num.py

--- pwtools/test/test_num.py
+++ pwtools/test/test_num.py
@@ -1,14 +1,19 @@
+from itertools import product
+
 import numpy as np
 from pwtools import num
+
 rand = np.random.rand
+
 
 def test_round_mult():
     assert num.round_up_next_multiple(144,8) == 144
     assert num.round_up_next_multiple(145,8) == 152
 
+
 def test_euler_matrix():
     # http://mathworld.wolfram.com/EulerAngles.html
-    
+
     # 0 degree rotation
     assert np.allclose(num.euler_matrix(0,0,0), np.identity(3))
 
@@ -23,21 +28,29 @@ def test_euler_matrix():
     # rotation about z
     vec = np.array([0,0,1.0])
     rr = rand(50)
-    for ri in rr: 
+    for ri in rr:
         rot = num.euler_matrix(ri*2*np.pi,0,0)
         assert (np.dot(rot,vec) == vec).all()
-    
+
     # rotation about x'
     vec = np.array([1.0,0,0])
     rr = rand(50)
-    for ri in rr: 
+    for ri in rr:
         rot = num.euler_matrix(0, ri*np.pi,0)
         assert (np.dot(rot,vec) == vec).all()
-    
+
     # rotation about z'
     vec = np.array([0,0,1.0])
     rr = rand(50)
-    for ri in rr: 
+    for ri in rr:
         rot = num.euler_matrix(0, 0, ri*2*np.pi)
         assert (np.dot(rot,vec) == vec).all()
 
+
+def test_inner_points_mask():
+    # ndim = dimension of the domain, works for > 3 of course, but this is
+    # just a test. ndim > 1 uses qhull. ndim==1 requires ordered points.
+    for ndim in [1,2,3]:
+        a = np.array([x for x in product([0,1,2,3],repeat=ndim)])
+        ai = a[num.inner_points_mask(a)]
+        assert (ai == np.array([x for x in product([1,2],repeat=ndim)])).all()
--- pwtools/test/test_polyfit.py
+++ pwtools/test/test_polyfit.py
@@ -1,7 +1,9 @@
 from itertools import product
+
 import numpy as np
 from pwtools import num
 from pwtools.test.tools import assert_all_types_equal
+
 
 def make_kwd_lst(*args):
     kwd_lst = []
@@ -15,28 +17,28 @@ def make_kwd_lst(*args):
 def test_polyfit():
     scale = [{'scale': False}, {'scale': True}]
     levels = [{}, {'levels': 2}]
-    degs = [{'deg': 2}, {'degrange': [2,3,4,5]}, 
+    degs = [{'deg': 2}, {'degrange': [2,3,4,5]},
             {'degmin': 1, 'degmax': 5}]
-    kwd_lst = make_kwd_lst(scale, levels, degs)        
+    kwd_lst = make_kwd_lst(scale, levels, degs)
     for kwds in kwd_lst:
         print(kwds)
         # 1D
-        x = np.linspace(-5,5,100) 
+        x = np.linspace(-5,5,100)
         y = x**2.0 - 1.0
         f = num.PolyFit1D(x,y,**kwds)
         assert np.allclose(y, f(x))
         assert np.allclose(2.0*x, f(x, der=1))
         assert np.allclose(2.0, f(x, der=2))
         assert np.allclose(-1.0, f(f.get_min()))
-        assert np.allclose(-1.0, f(f.get_min(xtol=1e-10)))                         
-        assert np.allclose(-1.0, f(f.get_min(x0=1.0, tol=1e-10)))                  
-        assert np.allclose(-1.0, f(f.get_min(xab=[-1,1], xtol=1e-10, rtol=1e-14))) 
-        
+        assert np.allclose(-1.0, f(f.get_min(xtol=1e-10)))
+        assert np.allclose(-1.0, f(f.get_min(x0=1.0, tol=1e-10)))
+        assert np.allclose(-1.0, f(f.get_min(xab=[-1,1], xtol=1e-10, rtol=1e-14)))
+
         # API: PolyFit1D __call__ arg: scalar, 1d, 2d
         for xs in [2.0, np.array([2.0]), np.array([[2.0]])]:
             assert np.allclose(3.0, f(xs))
             assert type(np.array([3.0])) == type(np.array([f(xs)]))
-        
+
         # API: 3rd arg is always 'deg'
         fit1 = num.polyfit(x[:,None],y,2)
         fit2 = num.polyfit(x[:,None],y,deg=2)
@@ -60,13 +62,13 @@ def test_polyfit():
         ret = f(x)
         for i in range(10):
             assert (ret == f(x)).all()
-    
-    
+
+
     scale = [{'scale': False}, {'scale': True}]
     levels = [{}, {'levels': 1}]
-    degs = [{'deg': 4}, {'degrange': [3,4]}, 
+    degs = [{'deg': 4}, {'degrange': [3,4]},
             {'degmin': 2, 'degmax': 5}]
-    kwd_lst = make_kwd_lst(scale, levels, degs)        
+    kwd_lst = make_kwd_lst(scale, levels, degs)
     for kwds in kwd_lst:
         print(kwds)
         # 2D
@@ -85,7 +87,7 @@ def test_polyfit():
         for xs in [np.array([4.0,3.0]), np.array([[4.0,3.0]])]:
             assert np.allclose(19.0, f(xs))
             assert type(np.array([19.0])) == type(np.array([f(xs)]))
-        
+
         # mix terms
         zz = xx**2.0 + yy**2.0 + xx*yy**2.0
         f = num.PolyFit(points, zz, **kwds)
@@ -99,17 +101,9 @@ def test_compare_numpy():
         yy2 = num.PolyFit1D(x, y, 3, scale=scale)(x)
         assert np.allclose(yy1, yy2)
 
-def test_inner_points_mask():
-    # ndim = dimension of the domain, works for > 3 of course, but this is
-    # just a test. ndim > 1 uses qhull. ndim==1 requires ordered points.
-    for ndim in [1,2,3]:
-        a = np.array([x for x in product([0,1,2,3],repeat=ndim)])
-        ai = a[num.inner_points_mask(a)]
-        assert (ai == np.array([x for x in product([1,2],repeat=ndim)])).all()
-
 def test_scale_get_min():
     # two local minima separated by a max at ~6.5
-    x = np.linspace(2,10,50) 
+    x = np.linspace(2,10,50)
     y = np.cos(x)+x*.2
     xlo = 2.94080857
     xhi = 9.2237442
@@ -119,7 +113,7 @@ def test_scale_get_min():
         assert np.allclose(f.get_min(x0=8), np.array([xhi]))
         f = num.PolyFit1D(x, y, deg=9, scale=scale)
         assert np.allclose(f.get_min(x0=4), xlo)
-        assert np.allclose(f.get_min(x0=8), xhi)       
+        assert np.allclose(f.get_min(x0=8), xhi)
 
 def test_api():
     # PolyFit's call method returns a scalar when given a "scalar" input (a
@@ -144,7 +138,7 @@ def test_api():
     #
     # Note that PolyFit1D.get_min() returns a float instead!
     #
-    # This has not been a problem, at least in tests, since 
+    # This has not been a problem, at least in tests, since
     # >>> np.allclose(1.0, array([1.0]))
     # True
     #
