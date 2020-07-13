a933e6be25afef19c6c1b896a1d5c8a98425c66b
Steve Schmerler <git@elcorto.com>
2020-07-13 22:00:47 +0200

    TST: test_interpol: make pytest's collection machinery happy

--- pwtools/test/test_interpol.py
+++ pwtools/test/test_interpol.py
@@ -1,19 +1,24 @@
 import tempfile
+import pickle
+
 import numpy as np
+
 from pwtools import mpl, num
 from pwtools.test import tools
-import pickle as pickle
 from .testenv import testdir
+
 
 def return_min(inter):
     # Return scalar minimum instead of array (5.0 instead of [5.0]).
     return inter(inter.get_min(maxfun=1e6, maxiter=1e2))[0]
 
+
 def dump(obj, testdir=testdir):
-    fn = tempfile.mktemp(dir=testdir, prefix='test_interpol')
+    fn = tempfile.mktemp(dir=testdir, prefix='pwtools_test_interpol_')
     fd = open(fn, 'wb')
     pickle.dump(obj, fd, 2)
     fd.close()
+
 
 def get_data():
     x = np.linspace(-5,5,20)
@@ -24,9 +29,16 @@ def get_data():
     tgt = np.array([  5.0 ,  30])
     return dd, tgt
 
+
 class TestInterpol2D:
-    def __init__(self):
-        self.dd, self.tgt = get_data()
+    # Oh come on!
+    #   PytestCollectionWarning: cannot collect test class 'TestInterpol2D' because
+    #   it has a __init__ constructor
+    # What!? This is Python, right? I mean, this here is valid code. Used to work
+    # with nose, thank you very much.
+##    def __init__(self):
+##        self.dd, self.tgt = get_data()
+    dd, tgt = get_data()
 
     def test_rbf_multi(self):
         inter = num.Interpol2D(dd=self.dd, what='rbf_multi')
