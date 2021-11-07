362baefb050b226d07fdaf7297edd230adc87816
Steve Schmerler <git@elcorto.com>
2021-11-07 01:05:18 +0100

    STY: black-ify rbf

--- pwtools/rbf.py
+++ pwtools/rbf.py
@@ -5,11 +5,13 @@ Radial Basis Function regression. See :ref:`rbf` for details.
 import math
 
 from pwtools import config
+
 JAX_MODE = config.use_jax
 
 if JAX_MODE:
     import jax.numpy as np
     from jax.config import config
+
     # Need double prec, else, the analytic derivs in Rbf.deriv() as well as
     # the autodiff version Rbf.deriv_jax() are rubbish.
     config.update("jax_enable_x64", True)
@@ -25,10 +27,13 @@ import scipy.linalg as linalg
 try:
     from sklearn.model_selection import RepeatedKFold
 except ImportError:
+
     class RepeatedKFold:
         def __init__(*args, **kwds):
-            raise NotImplementedError("Failed to import RepeatedKFold from "
-                                      "sklearn, not installed?")
+            raise NotImplementedError(
+                "Failed to import RepeatedKFold from "
+                "sklearn, not installed?"
+            )
 
 
 def rbf_gauss(rsq, p):
@@ -41,7 +46,7 @@ def rbf_gauss(rsq, p):
     p : float
         width
     """
-    return np.exp(-0.5*rsq / p**2.0)
+    return np.exp(-0.5 * rsq / p ** 2.0)
 
 
 def rbf_multi(rsq, p):
@@ -54,7 +59,7 @@ def rbf_multi(rsq, p):
     p : float
         width
     """
-    return np.sqrt(rsq + p**2.0)
+    return np.sqrt(rsq + p ** 2.0)
 
 
 def rbf_inv_multi(rsq, p):
@@ -67,14 +72,14 @@ def rbf_inv_multi(rsq, p):
     p : float
         width
     """
-    return 1/rbf_multi(rsq, p)
+    return 1 / rbf_multi(rsq, p)
 
 
 rbf_dct = {
-    'gauss': rbf_gauss,
-    'multi': rbf_multi,
-    'inv_multi': rbf_inv_multi,
-    }
+    "gauss": rbf_gauss,
+    "multi": rbf_multi,
+    "inv_multi": rbf_inv_multi,
+}
 
 
 # Consider using jax-md space.py here
@@ -95,11 +100,13 @@ def _np_distsq(aa, bb):
     >>> %timeit jf(x,x)
     1.03 ms ± 53.9 µs per loop (mean ± std. dev. of 7 runs, 1 loop each)
     """
-    return ((aa[:,None,:] - bb[None,...])**2.0).sum(-1)
+    return ((aa[:, None, :] - bb[None, ...]) ** 2.0).sum(-1)
+
 
 if JAX_MODE:
     _jax_np_distsq = jit(_np_distsq)
     _jax_np_dist = jit(lambda aa, bb: np.sqrt(_np_distsq(aa, bb)))
+
 
 def squared_dists(aa, bb):
     if JAX_MODE:
@@ -186,8 +193,10 @@ class Rbf:
     >>> ax2.set_xlabel('x'); ax2.set_ylabel('y');
     >>> ax2.legend()
     """
-    def __init__(self, points, values, rbf='inv_multi',
-                 r=None, p='mean', fit=True):
+
+    def __init__(
+        self, points, values, rbf="inv_multi", r=None, p="mean", fit=True
+    ):
         r"""
         Parameters
         ----------
@@ -205,23 +214,25 @@ class Rbf:
         fit : bool
             call :meth:`fit` in :meth:`__init__`
         """
-        assert points.ndim == 2, ("points must be 2d array")
-        assert values.ndim == 1, ("values must be 1d array")
+        assert points.ndim == 2, "points must be 2d array"
+        assert values.ndim == 1, "values must be 1d array"
         self.npoints = points.shape[0]
         self.ndim = points.shape[1]
-        assert len(values) == self.npoints, (f"{len(values)=} != {self.npoints=}")
+        assert (
+            len(values) == self.npoints
+        ), f"{len(values)=} != {self.npoints=}"
         self.points = points
         self.values = values
         self.rbf = rbf_dct[rbf] if isinstance(rbf, str) else rbf
         self.distsq = None
         if isinstance(p, str):
-            if p == 'mean':
+            if p == "mean":
                 # re-implement the 'mean' case here again since we can re-use
                 # distsq later (training data distance matrix)
                 self.distsq = self.get_distsq()
                 self.p = np.sqrt(self.distsq).mean()
-            elif p == 'scipy':
-                self.p = estimate_p(points, 'scipy')
+            elif p == "scipy":
+                self.p = estimate_p(points, "scipy")
             else:
                 raise ValueError("p is not 'mean' or 'scipy'")
         else:
@@ -233,7 +244,7 @@ class Rbf:
 
     def _rectify_points_shape(self, points):
         ret = np.atleast_2d(points)
-        assert ret.shape[1] == self.ndim, (f"{ret.shape[1]=} != {self.ndim}")
+        assert ret.shape[1] == self.ndim, f"{ret.shape[1]=} != {self.ndim}"
         return ret
 
     def get_distsq(self, points=None):
@@ -272,8 +283,7 @@ class Rbf:
             return squared_dists(points, self.points)
 
     def get_params(self):
-        """Return ``(p,r)``.
-        """
+        """Return ``(p,r)``."""
         return self.p, self.r
 
     def fit(self):
@@ -305,13 +315,13 @@ class Rbf:
             linear solver when ``self.r != None``.
         """
         G = self.rbf(self.get_distsq(), self.p)
-        assert G.shape == (self.npoints,)*2
+        assert G.shape == (self.npoints,) * 2
         # The lstsq solver is ~10x slower than jax' solver and ~4x slower than
         # the symmetric scipy solver
         if self.r is None:
             self.w = self._solve_lstsq(G)
         else:
-            self.w = self._solve(G + np.eye(G.shape[0])*self.r)
+            self.w = self._solve(G + np.eye(G.shape[0]) * self.r)
 
     def _solve_lstsq(self, G):
         x, res, rnk, svs = linalg.lstsq(G, self.values)
@@ -340,17 +350,17 @@ class Rbf:
         -------
         vals : 1d array (L,) or scalar
         """
-        _got_single_point = points.ndim==1
+        _got_single_point = points.ndim == 1
         points = self._rectify_points_shape(points)
-        assert points.shape[1] == self.ndim, ("wrong ndim")
+        assert points.shape[1] == self.ndim, "wrong ndim"
         G = self.rbf(self.get_distsq(points=points), self.p)
         assert G.shape[0] == points.shape[0]
-        assert G.shape[1] == len(self.w), \
-               "shape mismatch between g_ij: %s and w_j: %s, 2nd dim of "\
-               "g_ij must match length of w_j" %(str(G.shape),
-                                                 str(self.w.shape))
+        assert G.shape[1] == len(self.w), (
+            "shape mismatch between g_ij: %s and w_j: %s, 2nd dim of "
+            "g_ij must match length of w_j" % (str(G.shape), str(self.w.shape))
+        )
         # normalize w
-        maxw = np.abs(self.w).max()*1.0
+        maxw = np.abs(self.w).max() * 1.0
         values = np.dot(G, self.w / maxw) * maxw
         return values[0] if _got_single_point else values
 
@@ -384,10 +394,10 @@ class Rbf:
         """
         if JAX_MODE:
             if points.ndim == 1:
-                assert len(points) == self.ndim, ("wrong ndim")
+                assert len(points) == self.ndim, "wrong ndim"
                 return jit(grad(self))(points)
             elif points.ndim == 2:
-                assert points.shape[1] == self.ndim, ("wrong ndim")
+                assert points.shape[1] == self.ndim, "wrong ndim"
                 return jit(vmap(grad(self)))(points)
             else:
                 raise Exception("points has wrong shape")
@@ -460,24 +470,33 @@ class Rbf:
         #     for kk in range(N):
         #         vec = -1.0 * (centers[:,kk] - points[ll,kk]) / G[ll,:]
         #         D[ll,kk] = np.dot(vec, self.w)
-        _got_single_point = points.ndim==1
+        _got_single_point = points.ndim == 1
         points = self._rectify_points_shape(points)
-        assert points.shape[1] == self.ndim, ("wrong ndim")
-        L,N = points.shape
+        assert points.shape[1] == self.ndim, "wrong ndim"
+        L, N = points.shape
         centers = self.points
         G = self.rbf(self.get_distsq(points=points), self.p)
-        maxw = np.abs(self.w).max()*1.0
+        maxw = np.abs(self.w).max() * 1.0
         fname = self.rbf.__name__
 
+        # fmt: off
         # analytic deriv funcs for the inner loop
-        D_zz = dict(rbf_multi=lambda zz: -np.dot(((centers - points[zz,:]) / \
-                                         G[zz,:][:,None]).T, self.w / maxw) * maxw,
-                    rbf_inv_multi=lambda zz: np.dot(((centers - points[zz,:]) * \
-                                             (G[zz,:]**3.0)[:,None]).T, self.w / maxw) * maxw,
-                    rbf_gauss=lambda zz: 1.0 / self.p**2.0 * np.dot(((centers - points[zz,:]) \
-                                         * G[zz,:][:,None]).T, self.w / maxw) * maxw,
-                )
-        assert fname in D_zz.keys(), (f"{fname} not in {D_zz.keys()}")
+        D_zz = dict(
+            rbf_multi=lambda zz: -np.dot(
+                ((centers - points[zz, :]) / G[zz, :][:, None]).T,
+                self.w / maxw,
+            ) * maxw,
+            rbf_inv_multi=lambda zz: np.dot(
+                ((centers - points[zz, :]) * (G[zz, :] ** 3.0)[:, None]).T,
+                self.w / maxw,
+            ) * maxw,
+            rbf_gauss=lambda zz: 1.0 / self.p ** 2.0 * np.dot(
+                ((centers - points[zz, :]) * G[zz, :][:, None]).T,
+                self.w / maxw,
+            ) * maxw,
+        )
+        # fmt: on
+        assert fname in D_zz.keys(), f"{fname} not in {D_zz.keys()}"
         func = D_zz[fname]
         if JAX_MODE:
             # Of course don't call this method when doing jax autodiff. Still
@@ -502,15 +521,15 @@ class Rbf:
             ##    D.at[zz,:].set(func(zz))
             D = np.array([func(zz) for zz in range(L)])
         else:
-            D = np.empty((L,N), dtype=float)
+            D = np.empty((L, N), dtype=float)
             for zz in range(L):
-                D[zz,:] = func(zz)
+                D[zz, :] = func(zz)
         return D[0] if _got_single_point else D
 
     def fit_error(self, points, values):
         """Sum of squared fit errors with penalty on negative `p`."""
         res = values - self(points)
-        err = np.dot(res,res) / len(res)
+        err = np.dot(res, res) / len(res)
         return math.exp(abs(err)) if self.p < 0 else err
 
     def __call__(self, *args, **kwargs):
@@ -542,10 +561,10 @@ class Rbf:
         # >>> f=Rbf(X,y)
         # >>> jax.hessian(f)(rand(2)).shape
         # (4,4)
-        if 'der' in list(kwargs.keys()):
-            if kwargs['der'] != 1:
+        if "der" in list(kwargs.keys()):
+            if kwargs["der"] != 1:
                 raise Exception("only der=1 supported")
-            kwargs.pop('der')
+            kwargs.pop("der")
             if JAX_MODE:
                 return self.deriv_jax(*args, **kwargs)
             else:
@@ -554,7 +573,7 @@ class Rbf:
             return self.predict(*args, **kwargs)
 
 
-def estimate_p(points, method='mean'):
+def estimate_p(points, method="mean"):
     r"""Estimate :math:`p`.
 
     Parameters
@@ -563,15 +582,15 @@ def estimate_p(points, method='mean'):
         | 'mean' : :math:`1/M^2\,\sum_{ij} R_{ij}; M=\texttt{points.shape[0]}`
         | 'scipy' : mean nearest neighbor distance
     """
-    if method == 'mean':
+    if method == "mean":
         return euclidean_dists(points, points).mean()
-    elif method == 'scipy':
+    elif method == "scipy":
         xi = points.T
         ximax = np.amax(xi, axis=1)
         ximin = np.amin(xi, axis=1)
         edges = ximax - ximin
         edges = edges[np.nonzero(edges)]
-        return np.power(np.prod(edges)/xi.shape[-1], 1.0/edges.size)
+        return np.power(np.prod(edges) / xi.shape[-1], 1.0 / edges.size)
     else:
         raise Exception(f"illegal method: {method}")
 
@@ -597,8 +616,14 @@ class FitError:
     use :meth:`__call__` which will call one or the other, depending on
     `params`.
     """
-    def __init__(self, points, values, rbf_kwds=dict(),
-                 cv_kwds=dict(n_splits=5, n_repeats=1)):
+
+    def __init__(
+        self,
+        points,
+        values,
+        rbf_kwds=dict(),
+        cv_kwds=dict(n_splits=5, n_repeats=1),
+    ):
         """
         Parameters
         ----------
@@ -625,15 +650,19 @@ class FitError:
         points = self.points if points is None else points
         values = self.values if values is None else values
         if len(params) == 1:
-            assert 'p' not in self.rbf_kwds.keys(), "'p' in kwds"
+            assert "p" not in self.rbf_kwds.keys(), "'p' in kwds"
             return Rbf(points, values, p=params[0], **self.rbf_kwds)
         elif len(params) == 2:
-            for kw in ['p', 'r']:
+            for kw in ["p", "r"]:
                 assert kw not in self.rbf_kwds.keys(), f"'{kw}' in kwds"
-            return Rbf(points, values, p=params[0], r=params[1], **self.rbf_kwds)
+            return Rbf(
+                points, values, p=params[0], r=params[1], **self.rbf_kwds
+            )
         else:
-            raise Exception("length of params can only be 1 or 2, got "
-                            "{}".format(len(params)))
+            raise Exception(
+                "length of params can only be 1 or 2, got "
+                "{}".format(len(params))
+            )
 
     def cv(self, params):
         """K-fold repeated CV.
@@ -661,17 +690,20 @@ class FitError:
         errs : 1d array (n_repeats * n_splits,)
             direct fit error from each fold
         """
-        ns = self.cv_kwds['n_splits']
-        nr = self.cv_kwds['n_repeats']
-        errs = np.empty((ns*nr,), dtype=float)
+        ns = self.cv_kwds["n_splits"]
+        nr = self.cv_kwds["n_repeats"]
+        errs = np.empty((ns * nr,), dtype=float)
         folds = RepeatedKFold(**self.cv_kwds)
         for ii, tup in enumerate(folds.split(self.points)):
             idxs_train, idxs_test = tup
-            rbfi = self._get_rbfi(params,
-                                  self.points[idxs_train,...],
-                                  self.values[idxs_train,...])
-            errs[ii] = rbfi.fit_error(self.points[idxs_test,...],
-                                      self.values[idxs_test,...])
+            rbfi = self._get_rbfi(
+                params,
+                self.points[idxs_train, ...],
+                self.values[idxs_train, ...],
+            )
+            errs[ii] = rbfi.fit_error(
+                self.points[idxs_test, ...], self.values[idxs_test, ...]
+            )
         return errs
 
     def err_cv(self, params):
@@ -688,8 +720,15 @@ class FitError:
         return self._get_rbfi(params).fit_error(self.points, self.values)
 
 
-def fit_opt(points, values, method='de', what='pr', cv_kwds=dict(n_splits=5,
-            n_repeats=1), opt_kwds=dict(), rbf_kwds=dict()):
+def fit_opt(
+    points,
+    values,
+    method="de",
+    what="pr",
+    cv_kwds=dict(n_splits=5, n_repeats=1),
+    opt_kwds=dict(),
+    rbf_kwds=dict(),
+):
     """Optimize :class:`Rbf`'s hyper-parameter :math:`p` or both :math:`(p,r)`.
 
     Use a cross validation error metric or the direct fit error if `cv_kwds` is
@@ -713,37 +752,38 @@ def fit_opt(points, values, method='de', what='pr', cv_kwds=dict(n_splits=5,
     rbfi : :class:`Rbf`
         Rbf instance initialized with `points`, `values` and found optimal `p` (and `r`).
     """
-    assert what in ['p', 'pr'], (f"unknown `what` value: {what}")
-    assert method in ['de', 'fmin', 'brute'], (f"unknown `method` value: {method}")
+    assert what in ["p", "pr"], f"unknown `what` value: {what}"
+    assert method in [
+        "de",
+        "fmin",
+        "brute",
+    ], f"unknown `method` value: {method}"
     fit_err = FitError(points, values, cv_kwds=cv_kwds, rbf_kwds=rbf_kwds)
     p0 = estimate_p(points)
-    disp = opt_kwds.pop('disp', False)
-    if method == 'fmin':
-        if what == 'p':
-            x0 = opt_kwds.pop('x0', [p0])
-        elif what == 'pr':
-            x0 = opt_kwds.pop('x0', [p0, 1e-8])
-        xopt = optimize.fmin(fit_err, x0,
-                             disp=disp, **opt_kwds)
-    elif method in ['de', 'brute']:
-        if what == 'p':
-            _bounds = [(0, 5*p0)]
-        elif what == 'pr':
-            _bounds = [(0, 5*p0), (1e-12, 1e-1)]
-        if method == 'de':
-            bounds = opt_kwds.pop('bounds', _bounds)
-            ret = optimize.differential_evolution(fit_err,
-                                                  bounds=bounds,
-                                                  disp=disp,
-                                                  **opt_kwds)
+    disp = opt_kwds.pop("disp", False)
+    if method == "fmin":
+        if what == "p":
+            x0 = opt_kwds.pop("x0", [p0])
+        elif what == "pr":
+            x0 = opt_kwds.pop("x0", [p0, 1e-8])
+        xopt = optimize.fmin(fit_err, x0, disp=disp, **opt_kwds)
+    elif method in ["de", "brute"]:
+        if what == "p":
+            _bounds = [(0, 5 * p0)]
+        elif what == "pr":
+            _bounds = [(0, 5 * p0), (1e-12, 1e-1)]
+        if method == "de":
+            bounds = opt_kwds.pop("bounds", _bounds)
+            ret = optimize.differential_evolution(
+                fit_err, bounds=bounds, disp=disp, **opt_kwds
+            )
             xopt = ret.x
-        elif method == 'brute':
-            bounds = opt_kwds.pop('ranges', _bounds)
-            xopt = optimize.brute(fit_err,
-                                  ranges=bounds,
-                                  disp=disp,
-                                  **opt_kwds)
-    if what == 'pr':
+        elif method == "brute":
+            bounds = opt_kwds.pop("ranges", _bounds)
+            xopt = optimize.brute(
+                fit_err, ranges=bounds, disp=disp, **opt_kwds
+            )
+    if what == "pr":
         rbfi = Rbf(points, values, p=xopt[0], r=xopt[1])
     else:
         rbfi = Rbf(points, values, p=xopt[0])
