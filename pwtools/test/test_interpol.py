f20663059c06fe05db6bbd6717b2e92043afa6f9
Steve Schmerler <git@elcorto.com>
2022-04-25 13:36:35 +0200

    MNT: test_interpol: use np.testing.assert_allclose()

--- pwtools/test/test_interpol.py
+++ pwtools/test/test_interpol.py
@@ -42,30 +42,30 @@ class TestInterpol2D:
 
     def test_rbf_multi(self):
         inter = num.Interpol2D(dd=self.dd, what='rbf_multi')
-        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
-        assert np.allclose(return_min(inter), 5.0)
-        assert np.allclose(inter(self.dd.XY), self.dd.zz)
+        np.testing.assert_allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        np.testing.assert_allclose(return_min(inter), 5.0)
+        np.testing.assert_allclose(inter(self.dd.XY), self.dd.zz)
         dump(inter)
 
     def test_rbf_inv_multi(self):
         inter = num.Interpol2D(dd=self.dd, what='rbf_inv_multi')
-        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
-        assert np.allclose(return_min(inter), 5.0)
-        assert np.allclose(inter(self.dd.XY), self.dd.zz)
+        np.testing.assert_allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        np.testing.assert_allclose(return_min(inter), 5.0)
+        np.testing.assert_allclose(inter(self.dd.XY), self.dd.zz)
         dump(inter)
 
     def test_rbf_gauss(self):
         inter = num.Interpol2D(dd=self.dd, what='rbf_gauss')
-        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
-        assert np.allclose(return_min(inter), 5.0, atol=1e-5)
-        assert np.allclose(inter(self.dd.XY), self.dd.zz)
+        np.testing.assert_allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        np.testing.assert_allclose(return_min(inter), 5.0, atol=1e-5)
+        np.testing.assert_allclose(inter(self.dd.XY), self.dd.zz)
         dump(inter)
 
     def test_poly(self):
         inter = num.Interpol2D(dd=self.dd, what='poly', deg=5)
-        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
-        assert np.allclose(return_min(inter), 5.0, atol=1e-5)
-        assert np.allclose(inter(self.dd.XY), self.dd.zz)
+        np.testing.assert_allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        np.testing.assert_allclose(return_min(inter), 5.0, atol=1e-5)
+        np.testing.assert_allclose(inter(self.dd.XY), self.dd.zz)
         dump(inter)
 
     def test_bispl(self):
@@ -82,17 +82,17 @@ class TestInterpol2D:
         # interpolator again as needed. Not a speed problem, just inconvenient. All
         # other interpolator types can be pickled.
         inter = num.Interpol2D(dd=self.dd, what='bispl')
-        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
-        assert np.allclose(return_min(inter), 5.0)
-        assert np.allclose(inter(self.dd.XY), self.dd.zz)
+        np.testing.assert_allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        np.testing.assert_allclose(return_min(inter), 5.0)
+        np.testing.assert_allclose(inter(self.dd.XY), self.dd.zz)
         ##dump(inter)
 
     def test_linear(self):
         # linear, ct and nearest are very inaccurate, use only for plotting!
         inter = num.Interpol2D(dd=self.dd, what='linear')
-        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt, atol=5e-1)
-        assert np.allclose(return_min(inter), 5.0, atol=1e-1)
-        assert np.allclose(inter(self.dd.XY), self.dd.zz)
+        np.testing.assert_allclose(inter([[-3,-4],[0,0]]), self.tgt, atol=5e-1)
+        np.testing.assert_allclose(return_min(inter), 5.0, atol=1e-1)
+        np.testing.assert_allclose(inter(self.dd.XY), self.dd.zz)
         dump(inter)
 
     def test_nearest(self):
@@ -104,10 +104,10 @@ class TestInterpol2D:
         try:
             from scipy.interpolate import CloughTocher2DInterpolator
             inter = num.Interpol2D(dd=self.dd, what='ct')
-            assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt,
+            np.testing.assert_allclose(inter([[-3,-4],[0,0]]), self.tgt,
                 atol=1e-1)
-            assert np.allclose(return_min(inter), 5.0, atol=1e-1)
-            assert np.allclose(inter(self.dd.XY), self.dd.zz)
+            np.testing.assert_allclose(return_min(inter), 5.0, atol=1e-1)
+            np.testing.assert_allclose(inter(self.dd.XY), self.dd.zz)
             dump(inter)
         except ImportError:
             tools.skip("couldn't import scipy.interpolate.CloughTocher2DInterpolator")
@@ -115,13 +115,13 @@ class TestInterpol2D:
     def test_api_bispl(self):
         # API
         inter = num.Interpol2D(xx=self.dd.xx, yy=self.dd.yy, values=self.dd.zz, what='bispl')
-        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
-        assert np.allclose(return_min(inter), 5.0)
+        np.testing.assert_allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        np.testing.assert_allclose(return_min(inter), 5.0)
 
         inter = num.Interpol2D(points=self.dd.XY, values=self.dd.zz, what='bispl')
-        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
-        assert np.allclose(return_min(inter), 5.0)
+        np.testing.assert_allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        np.testing.assert_allclose(return_min(inter), 5.0)
 
         inter = num.Interpol2D(self.dd.XY, self.dd.zz, what='bispl')
-        assert np.allclose(inter([[-3,-4],[0,0]]), self.tgt)
-        assert np.allclose(return_min(inter), 5.0)
+        np.testing.assert_allclose(inter([[-3,-4],[0,0]]), self.tgt)
+        np.testing.assert_allclose(return_min(inter), 5.0)
